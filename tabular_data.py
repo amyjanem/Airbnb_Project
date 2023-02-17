afa33c010a9833fbf189090fcf8991b1e27be8b1
Amy <amymallett8@gmail.com>
2023-02-17 18:17:12 +0000

    edits to datapreparation class

--- tabular_data.py
+++ tabular_data.py
@@ -2,25 +2,47 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import missingno as msno
+from ast import literal_eval
 
-#print(np.sort(df['beds'].unique()))
-
-#print(df.iloc[1])
-#print(df.info())
-
-#df_null = df.isnull()
-#print(df_null)
-#print(df_null.sum())
-#print(df_null.mean()*100)
-
-#msno.matrix(df)
+class DataPreparation:
 
 
-def remove_rows_with_missing_ratings(dataset = "tabular_data\listing.csv", ratings_columns = ("Cleanliness_rating", "Accuracy_rating", "Communication_rating", "Location_rating", "Check-in_rating", "Value_rating")):
+    def __init__(self) -> None:
+            self.data_file_path = "tabular_data\listing.csv"
+            self.df = pd.read_csv(self.data_file_path)
 
-    df = pd.read_csv(dataset)
-    df_missing_ratings_removed = df.dropna(subset=[ratings_columns])
+        
+    def remove_rows_with_missing_ratings(self, ratings_columns = ["Description", "Cleanliness_rating", "Accuracy_rating", "Communication_rating", "Location_rating", "Check-in_rating", "Value_rating"]) -> pd.DataFrame:
 
-    return df_missing_ratings_removed
+        df = self.df.dropna(subset=ratings_columns)
 
-remove_rows_with_missing_ratings()
+        return df
+
+
+    def combine_description_strings(self):
+        
+        #remove empty quotes
+        #join the list elements
+        literal_eval
+        
+
+        #remove 'about this space' prefix
+
+
+        # for item in df["Description"]:
+        #     item.remove('About this space')
+
+
+        #df["Description"] = df["Description"].remove('About this space')
+        #df['Description'] = df['Description'].apply(lambda x: ' '.join(x) if type(x)==list else x)
+        
+        #print(df["Description"].head())
+    
+        return df
+
+    #combine_description_strings(df)
+
+
+
+dataprep = DataPreparation()  
+dataprep.remove_rows_with_missing_ratings()  
